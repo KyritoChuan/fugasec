e9f1e58f841a3f0f6348e671e7cca8581afe5c66
Camilo Vallejos <camilo.vallejos@cencosudscotiabank.cl>
2024-11-18 14:09:37 -0300

    add initial template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://kyritochuan.github.io",
     base: "fugasec",
+    integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
